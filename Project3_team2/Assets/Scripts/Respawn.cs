15b3bfa1fde1477b175a885ca4cb1b6eddc8eeca
Cameron Cruz <lacedillionloptet@gmail.com>
2026-04-12 21:36:07 -0400

    added kill zones, checkpoints, respawn, and kill heights

--- Project3_team2/Assets/Scripts/Respawn.cs
+++ Project3_team2/Assets/Scripts/Respawn.cs
@@ -2,15 +2,55 @@ using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public float killHeight = -10f;
+
+    public Transform currentRespawnPoint;
+    public Transform startSpawn;
+
+    private Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        if (currentRespawnPoint == null && startSpawn != null)
+            currentRespawnPoint = startSpawn;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void FixedUpdate()
+    {
+        if (transform.position.y < killHeight)
+        {
+            RespawnPlayer();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RespawnPlayer()
     {
-        
+        if (currentRespawnPoint == null)
+        {
+            Debug.LogWarning("No respawn point set!");
+            return;
+        }
+        transform.position = currentRespawnPoint.position;
+        transform.rotation = currentRespawnPoint.rotation;
+
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+
+
+
     }
+
+    public void SetRespawnPoint(Transform newPoint)
+    {
+        currentRespawnPoint = newPoint;
+        Debug.Log("Checkpoint: " + newPoint.name);
+    }
+
 }
